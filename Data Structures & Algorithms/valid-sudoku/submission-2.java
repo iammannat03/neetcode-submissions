2c169b99aa1ee19f1d33e1026fcff0974cd2be40
Mannat Jaiswal <136169518+iammannat03@users.noreply.github.com>
2026-07-31 23:59:35 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char val = board[r][c];
+                if(val=='.') continue;
+
+                int num = val-'1';
+                int boxIndex = ((r/3)*3) + (c/3);
+
+                if(row[r][num] || col[c][num] || box[boxIndex][num]){
+                    return false;
+                }
+
+                row[r][num]=true;
+                col[c][num]=true;
+                box[boxIndex][num]=true;
+            }
+        }
+        return true;
+    }
+}
